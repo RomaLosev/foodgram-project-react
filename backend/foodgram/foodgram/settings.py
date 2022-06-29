2495c4039fcd8eb1891679d06033c8b4f12a93fd
Роман Лосев <losik19-94@yandex.ru>
2022-06-29 20:09:08 +0300

    settings fix url

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -162,10 +162,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media_backend/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
