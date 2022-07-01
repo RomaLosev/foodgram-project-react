6fbf1976cad135f1d2045ace35306f39c9fe4bea
Роман Лосев <losik19-94@yandex.ru>
2022-07-01 22:54:25 +0300

    recipes work

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-from foodgram.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
