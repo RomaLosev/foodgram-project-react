11f354480e705ffa896729ff02c07ba1da1c3dc0
Роман Лосев <losik19-94@yandex.ru>
2022-07-02 20:12:42 +0300

    filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -25,10 +25,10 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(carts__user=self.request.user)
         return queryset
